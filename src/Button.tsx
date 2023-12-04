4298061090743789d556808205fdade3db6d3acd
Milad Sadeghi <miladsadeghidm@gmail.com>
2023-12-04 08:40:14 +0330

    Specifying the type of function parametrs

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,5 +1,9 @@
-let url = "https://miladsa.de";
-url = 2;
+// let url = "https://miladsa.de";
+// url = 2;
+
+function convertCurrency(amount: number, currency: string) {
+  // Some code
+}
 
 export default function Button() {
   return <></>;
