9ec843ba9ab9e6abd24c3d11dede1ca38a0c751c
Milad Sadeghi <miladsadeghidm@gmail.com>
2023-12-04 08:49:17 +0330

    Specifying the function return type

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,7 +1,7 @@
 // let url = "https://miladsa.de";
 // url = 2;
 
-function convertCurrency(amount: number, currency: string) {
+function convertCurrency(amount: number, currency: string): void {
   // Some code
 }
 
