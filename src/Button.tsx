8e2d5a54e601cde6817e97b04a3d20c194213801
Milad Sadeghi <miladsadeghidm@gmail.com>
2023-12-05 20:53:47 +0330

    Wrapping button props styles

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,22 +1,20 @@
 type ButtonProps = {
   label: "click" | "Click on Me";
-  backgroundColor: "pink" | "gray" | "red";
-  padding: [string, string, string, string];
+  style: {
+    backgroundColor: "pink" | "gray" | "red";
+    padding: [string, string, string, string];
+  };
 };
 
-export default function Button({
-  label,
-  backgroundColor,
-  padding,
-}: ButtonProps) {
+export default function Button({ style }: ButtonProps) {
   return (
     <button
       style={{
-        backgroundColor: backgroundColor,
-        padding: `${padding[0]} ${padding[1]} ${padding[2]} ${padding[3]}`,
+        backgroundColor: style.backgroundColor,
+        padding: `${style.padding[0]} ${style.padding[1]} ${style.padding[2]} ${style.padding[3]}`,
       }}
     >
-      {label}
+      Click on Me
     </button>
   );
 }
--- src/Page.tsx
+++ src/Page.tsx
@@ -4,9 +4,11 @@ export default function Page() {
   return (
     <main>
       <Button
-        label="Click on Me"
-        backgroundColor="pink"
-        padding={["5px", "20px", "40px", "60px"]}
+        label={"Click on Me"}
+        style={{
+          backgroundColor: "pink",
+          padding: ["5px", "20px", "40px", "60px"],
+        }}
       />
     </main>
   );
