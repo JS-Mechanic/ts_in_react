9689d3ff6e5bbd2368b148f117f2ff93ff51f038
Milad Sadeghi <miladsadeghidm@gmail.com>
2023-12-05 20:42:25 +0330

    Completing button props

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,9 +1,22 @@
 type ButtonProps = {
   label: "click" | "Click on Me";
   backgroundColor: "pink" | "gray" | "red";
-  pillShape?: boolean;
+  padding: [string, string, string, string];
 };
 
-export default function Button({ label, backgroundColor }: ButtonProps) {
-  return <button style={{ backgroundColor: backgroundColor }}>{label}</button>;
+export default function Button({
+  label,
+  backgroundColor,
+  padding,
+}: ButtonProps) {
+  return (
+    <button
+      style={{
+        backgroundColor: backgroundColor,
+        padding: `${padding[0]} ${padding[1]} ${padding[2]} ${padding[3]}`,
+      }}
+    >
+      {label}
+    </button>
+  );
 }
--- src/Page.tsx
+++ src/Page.tsx
@@ -3,7 +3,11 @@ import Button from "./Button.tsx";
 export default function Page() {
   return (
     <main>
-      <Button label="Click on Me" backgroundColor="pink" />
+      <Button
+        label="Click on Me"
+        backgroundColor="pink"
+        padding={["5px", "20px", "40px", "60px"]}
+      />
     </main>
   );
 }
