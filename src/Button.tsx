a73dec1abba5cabc6d1f33640a936f22f7fcdc38
Milad Sadeghi <miladsadeghidm@gmail.com>
2023-12-08 20:41:57 +0330

    Clearing everything to test other features properly

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,24 +1,3 @@
-import { ComponentPropsWithoutRef } from "react";
-
-type ButtonProps = ComponentPropsWithoutRef<"button"> & {
-  variant?: "primary" | "secondary";
-};
-
-type SuperButtonProps = ButtonProps & { size: "small" | "medium" | "large" };
-
-export default function Button({
-  children,
-  variant,
-  autoFocus,
-  type,
-  ...rest
-}: SuperButtonProps) {
-  return (
-    <button autoFocus={autoFocus} type={type} {...rest}>
-      <>
-        {children}
-        <div>{variant}</div>
-      </>
-    </button>
-  );
+export default function Button() {
+  return <button>Click me</button>;
 }
--- src/Page.tsx
+++ src/Page.tsx
@@ -3,14 +3,7 @@ import Button from "./Button.tsx";
 export default function Page() {
   return (
     <main>
-      <Button
-        variant="secondary"
-        autoFocus={true}
-        type={"submit"}
-        style={{ backgroundColor: "pink", borderRadius: "5px 15px 25px 35px" }}
-      >
-        Click Here
-      </Button>
+      <Button />
     </main>
   );
 }
