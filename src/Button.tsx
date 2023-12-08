8303786919070899991a1f224ae769d348edef23
Milad Sadeghi <miladsadeghidm@gmail.com>
2023-12-08 20:45:36 +0330

    Typing event handlers in typescript

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,3 +1,9 @@
+import React from "react";
+
 export default function Button() {
-  return <button>Click me</button>;
+  function handleClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    console.log(`Button clicked ---> ${e.target}`);
+  }
+
+  return <button onClick={handleClick}>Click me</button>;
 }
