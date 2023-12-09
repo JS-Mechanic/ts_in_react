b634911735dd752afd0c7e73f2f1b94371eba7d8
Milad Sadeghi <miladsadeghidm@gmail.com>
2023-12-09 18:41:42 +0330

    Typing useState and useEffect hooks

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,9 +1,20 @@
 import Button from "./Button.tsx";
+import { useEffect, useState } from "react";
+
+type User = { name: string; age: number };
 
 export default function Page() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    setUser({ name: "Milad", age: 28 });
+  }, []);
+
   return (
     <main>
       <Button />
+      <p>User's name: {user?.name}</p>
+      <p>User's age: {user?.age}</p>
     </main>
   );
 }
