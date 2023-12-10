90ceb76b9c09b6e79f8123449d87080c365f31ee
Milad Sadeghi <miladsadeghidm@gmail.com>
2023-12-10 13:20:57 +0330

    Clearing

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,20 +1,3 @@
-type ButtonProps<T> = {
-  countValue: T;
-  countHistory: T[];
-};
-export default function Button<T>({
-  countValue,
-  countHistory,
-}: ButtonProps<T>) {
-  return (
-    <>
-      <button>Click on me</button>
-      <p>{countValue.toString()}</p>
-      <p>
-        {countHistory.reduce((acc, item) => {
-          return acc + item.toString();
-        }, "")}
-      </p>
-    </>
-  );
+export default function Button() {
+  return <button>Click on me</button>;
 }
--- src/Page.tsx
+++ src/Page.tsx
@@ -3,7 +3,7 @@ import Button from "./Button.tsx";
 export default function Page() {
   return (
     <main>
-      <Button countValue={5} countHistory={[1, 2, 3]} />
+      <Button />
     </main>
   );
 }
